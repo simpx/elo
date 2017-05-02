8c4439d88495ee51f4662a7d7cee944d8a6ba946
simpx <simpxx@gmail.com>
2017-05-02 10:07:39 +0800

    add left

--- main.py
+++ main.py
@@ -10,6 +10,7 @@ pp = pprint.PrettyPrinter(indent=4)
 parsed_league = {}
 parsed_teams = {}
 parsed_games = []
+left_games = []
 ranks = {}
 
 '''
@@ -20,6 +21,9 @@ def find_between(string, from_str, to_str):
 
 def team_name(team_id):
     return parsed_teams[team_id]["name"]
+
+def get_rank(team_id):
+    return ranks.get(team_id, 1000)
 
 # 解析联赛数据
 file_data = open("s36.js").readlines()
@@ -33,11 +37,12 @@ data_teams = eval(data_teams_str)
 for team in data_teams:
     parsed_teams[team[0]] = {"name": team[1], "eng": team[3]}
 
+file_data = file_data[2:]
 x = 0
 # 解析赛事数据
-for i in range(2, 2 + 38):
-    if not file_data[i].strip().startswith("jh[\"R_%d\"" % (i - 1)):
-        print "should starts with %s , but: %s" % ( ("jh[\"R_%d\"" % (i - 1)), file_data[i])
+for i in range(0, 38):
+    if not file_data[i].strip().startswith("jh[\"R_%d\"" % (i + 1)):
+        print "should starts with %s , but: %s" % ( ("jh[\"R_%d\"" % (i + 1)), file_data[i])
         sys.exit(-1)
     round_str = file_data[i].strip().split(' ', 2)[2][:-1]
     round_str = round_str.replace(",,,", ",'x','x',", -1).replace(",,", ",'x',", -1)
@@ -49,19 +54,31 @@ for i in range(2, 2 + 38):
             second_half_host_score, second_half_guest_score = [int(x) for x in game[6].split('-')]
             host_score = first_half_host_score + second_half_host_score
             guest_score = first_half_guest_score + second_half_guest_score
-            result = 0.5
             if host_score > guest_score:
                 result = 1
             elif host_score < guest_score:
                 result = 0
-            data = {"host_id": game[4], "guest_id": game[5],
-                    "host_score": host_score, "guest_score": guest_score,
-                    "first_half_host_score": first_half_host_score,
-                    "first_half_guest_score": first_half_guest_score,
-                    "second_half_host_score": second_half_host_score,
-                    "second_half_guest_score": second_half_guest_score,
-                    "round": idx + 1, "result": result}
+            else:
+                result = 0.5
+        else:
+            first_half_host_score, first_half_guest_score = [-1, -1]
+            second_half_host_score, second_half_guest_score = [-1, -1]
+            host_score = -1
+            guest_score = -1
+            result = -1
+
+        data = {"host_id": game[4], "guest_id": game[5],
+                "host_score": host_score, "guest_score": guest_score,
+                "first_half_host_score": first_half_host_score,
+                "first_half_guest_score": first_half_guest_score,
+                "second_half_host_score": second_half_host_score,
+                "second_half_guest_score": second_half_guest_score,
+                "round": i + 1, "result": result, "start_time": game[3]}
+        if game[2] == -1:
             parsed_games.append(data)
+        else:
+            left_games.append(data)
+
             
 # 展示所有数据
 
@@ -86,8 +103,8 @@ for game in parsed_games:
     guest_id = game["guest_id"]
     host_name = team_name(host_id)
     guest_name = team_name(guest_id)
-    host_old_rank = ranks.get(host_id, 1000)
-    guest_old_rank = ranks.get(guest_id, 1000)
+    host_old_rank = get_rank(host_id)
+    guest_old_rank = get_rank(guest_id)
     host_score = game["host_score"]
     guest_score = game["guest_score"]
 
@@ -108,6 +125,16 @@ for game in parsed_games:
                                                 host_new_rank, guest_new_rank)
 
 print "-----------------------------"
+print "ranks: "
 sorted_ranks = sorted(ranks.items(), key=operator.itemgetter(1), reverse=True)
 for rank in sorted_ranks:
-    print "%s (%.2f)" % (team_name(rank[0]), rank[1])
+    print "%-20s (%.2f)" % (team_name(rank[0]), rank[1])
+
+print "-----------------------------"
+print "left: "
+
+for game in left_games:
+    print "%2d %s %-20s(%.2f) vs %-20s(%.2f) %.2f%%" % (game['round'], game['start_time'],
+            team_name(game['host_id']), get_rank(game['host_id']),
+            team_name(game['guest_id']), get_rank(game['guest_id']),
+            100 * elo.E(get_rank(game['host_id']), get_rank(game['guest_id'])))
